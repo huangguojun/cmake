e27f2acb4b8bf67829fc4885163759e7e89056c5
guojun.huang <guojun.huang@qq.com>
2017-05-24 20:22:02 +0800

    update

---
+++ t5/build/src/CMakeFiles/Test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test.dir/test1.cpp.o"
+  "libTest.pdb"
+  "libTest.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ t5/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2017年05月18日 11时43分49秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  guojun.huang@qq.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+#include<iostream>
+ 
+int main()
+{
+	std::cout<<"Hello word!"<<std::endl;
+	return 0;
+}
