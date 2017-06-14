16687a3136cd7843de2e7ce2e82440511009588e
guojun.huang <guojun.huang@qq.com>
2017-06-14 13:41:09 +0800

    update 6.14  13:44

--- t6/cmake/modules/Findlibdb_cxx.cmake
+++ t6/cmake/modules/Findlibdb_cxx.cmake
@@ -1,13 +1,14 @@
-MESSAGE(STATUS "Using bundled Findlibdb.cmake...")
+MESSAGE(STATUS "Using bundled Findlibdb_cxx.cmake...")
+
 FIND_PATH(
 	LIBDB_CXX_INCLUDE_DIR
 	db_cxx.h 
 	/usr/include/ 
 	/usr/local/include/ 
-)
- 
+ )
+
 FIND_LIBRARY(
-	LIBDB_CXX_LIBRARIES NAMES  db_cxx
+	LIBDB_CXX_LIBRARIES 
+	NAMES  db_cxx
 	PATHS /usr/lib/ /usr/local/lib/
 )
-
