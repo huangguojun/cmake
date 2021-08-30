312fe932c477a043ca1c9047567853dbad357194
guojun.huang <guojun.huang@qq.com>
2021-08-31 00:56:50 +0800

    update

---
+++ clang-blueprint/src/main.cpp
@@ -0,0 +1,17 @@
+/// \file
+
+#include <bits/stdint-intn.h>
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+
+#include "fact/factorial.h"
+
+/// Program entry point.
+int32_t main(const int32_t /*argc*/, const char* /*argv[]*/) {
+    const int32_t n = 7;
+    const int32_t result = fact::factorial(n);
+
+    std::cout << "Hello, World! factorial(" << n << ") = " << result << std::endl;
+    return int32_t(0);
+}
